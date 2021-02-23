dad671ea3519daec4d94266ec0287f6ff4f166b2
fvilante <fvilante1@gmail.com>
2021-02-23 04:48:25 -0300

    Added some Molds of IVECO and the SCANIA V120

--- src/serial-port/known-jobs.ts
+++ src/serial-port/known-jobs.ts
@@ -43,6 +43,37 @@ namespace Draft {
 // ================== end temp draft =================
 
 
+type XYDelta = {
+    x: Milimeter, // milimeter in relation to white Head
+    y: Milimeter, // milimeter in relation to cmpp 0
+}
+
+const deltaHead: XYDelta = {
+    x: Milimeter(-46.51),
+    y: Milimeter(+5.90),
+}
+
+const deltaGaveta2: XYDelta = {
+    x: Milimeter(+603.5+1.29),
+    y: Milimeter(+6.34+3.98+1.23),
+}
+
+const deltaId: XYDelta = {
+    x: Milimeter(0),
+    y: Milimeter(0),
+}
+
+const applyDeltaToCoordinates = (xss: ImpressoesX, ys: readonly number[], delta: XYDelta): [newXs: ImpressoesX, newYs: readonly number[]] => {
+    const { x: xHead, y: yHead} = delta
+    const newYs = ys.map( y => y + yHead.value)
+    const newXs = xss.map( xs => xs.map( x => x + xHead.value)) as unknown as ImpressoesX
+    console.log('ys:', ys)
+    console.log('new_ys:', newYs)
+    console.log('xs', xss)
+    console.log('newXs', newXs)
+    return [newXs, newYs]
+}
+
 export type Job__ = {
     // Proxy
     partNumber: string
@@ -89,13 +120,17 @@ export type KnownJobs = {
     'T123': () => Job__
     'V2': () => Job__
     'T202': () => Job__
+    'V120': () => Job__
 
     // iveco
     'ST18': () => Job__
     '25002 B': () => Job__
+    '25402 B': () => Job__
+    '25006 A': () => Job__
     'ST22': () => Job__
     'ST6': () => Job__
     'ST93': () => Job__
+    'ST19': () => Job__
 }
 
 export const getKnownJobs = ():KnownJobs => {    
@@ -117,13 +152,17 @@ export const getKnownJobs = ():KnownJobs => {
         'T123': getT123Job,
         'V2': getV2Job,
         'T202': getT202Job,
+        'V120': getV120Job,
 
         // iveco
         'ST18': getST18job,
-        '25002 B': get2502Bjob,
+        '25002 B': get25002Bjob,
+        '25402 B': get25402Bjob,
+        "25006 A": get25006Ajob,
         'ST22': getST22Job,
         'ST6': getST6Job,
         'ST93': getST93Job,
+        'ST19': getST19Job,
     }
     
 }
@@ -203,10 +242,40 @@ const getST6Job = ():Job__ => {
     } 
 }
 
-const getST22Job = ():Job__ => {
-    const firstX = 155-9.5-6+4.8+2.5
+const getST19Job = (): Job__ => {
+    const firstX = 155-9.5-6+4.8+2.5+46.51+6.13
     const stepX = 70
-    const posicaoYDaLinha5EmMilimetros = 336+2-1
+    const posicaoYDaLinha5EmMilimetros = 336+2-1-5.9+2
+    const stepY = 70
+    return {
+        partNumber: '',
+        barCode: '',
+        printer: 'printerBlack',
+        msg: 'ST19',
+        remoteFieldId: 2,
+        printVelocity: 1700,
+        zLevel: 0,
+        impressoesX: [
+            [firstX+(stepX*0),firstX+(stepX*1)],
+            [firstX+(stepX*2),firstX+(stepX*3)],
+            [firstX+(stepX*4),firstX+(stepX*5)],
+        ],
+        linhasY: [
+            posicaoYDaLinha5EmMilimetros+(stepY*(2)),
+            posicaoYDaLinha5EmMilimetros+(stepY*(1)),
+            posicaoYDaLinha5EmMilimetros+(stepY*(0)),
+            posicaoYDaLinha5EmMilimetros+(stepY*(-1)),
+            posicaoYDaLinha5EmMilimetros+(stepY*(-2)),
+            posicaoYDaLinha5EmMilimetros+(stepY*(-3)),
+            posicaoYDaLinha5EmMilimetros+(stepY*(-4)),
+        ]
+    } 
+}
+
+const getST22Job = ():Job__ => {
+    const firstX = 155-9.5-6+4.8+2.5  
+    const stepX = 70
+    const posicaoYDaLinha5EmMilimetros = 336+2-1    
     const stepY = 70
     return {
         partNumber: '',
@@ -233,8 +302,22 @@ const getST22Job = ():Job__ => {
     } 
 }
 
+const get25402Bjob = (): Job__ => {
+    return {
+        ...get25002Bjob(),
+        msg: '25402 B'
+    }
+}
 
-const get2502Bjob = ():Job__ => {
+const get25006Ajob = (): Job__ => {
+    return {
+        ...get25002Bjob(),
+        msg: '25006 A'
+    }
+}
+
+
+const get25002Bjob = ():Job__ => {
     const firstX = 155-9.5-6+4.8
     const stepX = 70
     const posicaoYDaLinha5EmMilimetros = 336+2-1
@@ -291,6 +374,41 @@ const getST18job = ():Job__ => {
             posicaoYDaLinha5EmMilimetros+(stepY*(-3)),
             posicaoYDaLinha5EmMilimetros+(stepY*(-4)),
         ]
+    } 
+}
+
+
+const getV120Job = (): Job__ => {
+    const firstX = 150+13.66-8.15-5-3+1.5
+    const stepX = 70
+    const posicaoYDaLinha5EmMilimetros = 150+220-10-10+3-2-2.6+1.5-8.26-3.11+1.18+17
+    const impressoesX: ImpressoesX = [
+        [firstX+(stepX*0),firstX+(stepX*1)],
+        [firstX+(stepX*2),firstX+(stepX*3)],
+        [firstX+(stepX*4),firstX+(stepX*5)],
+    ]
+    const stepY = 70
+    const linhasY = [ // em milimetros absolutos
+        posicaoYDaLinha5EmMilimetros+(stepY*(2)),
+        posicaoYDaLinha5EmMilimetros+(stepY*(1)),
+        posicaoYDaLinha5EmMilimetros+(stepY*(0)),
+        posicaoYDaLinha5EmMilimetros+(stepY*(-1)),
+        posicaoYDaLinha5EmMilimetros+(stepY*(-2)),
+        posicaoYDaLinha5EmMilimetros+(stepY*(-3)),
+        posicaoYDaLinha5EmMilimetros+(stepY*(-4)),
+    ]
+    return {
+        partNumber: '',
+        printer: 'printerWhite',
+        barCode: '',
+        msg:  'V120',
+        remoteFieldId: 2,
+        impressoesX,
+        linhasY,
+        printVelocity: 1700,
+        zLevel:0,
+        passes: 2
+        
     } 
 }
 
@@ -568,37 +686,6 @@ const getE44A2Job = (): Job__ => {
 }
 
 const getE44B6Job = (): Job__ => {
-
-    type XYDelta = {
-        x: Milimeter, // milimeter in relation to white Head
-        y: Milimeter, // milimeter in relation to cmpp 0
-    }
-
-    const deltaHead: XYDelta = {
-        x: Milimeter(-46.51),
-        y: Milimeter(+5.90),
-    }
-
-    const deltaGaveta2: XYDelta = {
-        x: Milimeter(+603.5+1.29),
-        y: Milimeter(+6.34+3.98+1.23),
-    }
-
-    const deltaId: XYDelta = {
-        x: Milimeter(0),
-        y: Milimeter(0),
-    }
-
-    const applyDeltaToCoordinates = (xss: ImpressoesX, ys: readonly number[], delta: XYDelta): [newXs: ImpressoesX, newYs: readonly number[]] => {
-        const { x: xHead, y: yHead} = delta
-        const newYs = ys.map( y => y + yHead.value)
-        const newXs = xss.map( xs => xs.map( x => x + xHead.value)) as unknown as ImpressoesX
-        console.log('ys:', ys)
-        console.log('new_ys:', newYs)
-        console.log('xs', xss)
-        console.log('newXs', newXs)
-        return [newXs, newYs]
-    }
 
     const E44_A2 = getE44A2Job()
     const {
