e9efbd1c559fd808e87f1aaed427634677eeecec
fvilante <fvilante1@gmail.com>
2021-03-05 13:25:34 -0300

    Before this change, only the MSG was considered in the barcode readings, now PartNumber and Message are considered.

--- src/serial-port/main3.ts
+++ src/serial-port/main3.ts
@@ -1,16 +1,54 @@
 import { readKeyboardAsync } from "./read-keyboard-async"
-import { GetBarCodeFromSignal } from './parse-barcode'
-import { makeMovimentKit } from "./machine-controler"
-import { performMatrizByItsMsg } from "./matriz-router"
+import { BarCode, GetBarCodeFromSignal } from './parse-barcode'
+import { makeMovimentKit, MovimentKit } from "./machine-controler"
+import { performMatriz, performMatrizByItsMsg } from "./matriz-router"
+import { fetchMatrizByBarcodeRaw } from "./matriz-cadastro-geral-reader"
+import { createTerminal } from 'terminal-kit'
+import { Matriz } from "./matrizes-conhecidas"
+import { delay } from "../utils/delay"
 
+const term = createTerminal()
 
+// helper
+const performMatrizByItsBarCode = async (barCodeRaw: BarCode['raw'], movimentKit: MovimentKit): Promise<void> => {
+    const matrizes = await fetchMatrizByBarcodeRaw(barCodeRaw)
+    const matriz = await desambiguateSingleBarCodeMultipleRegistries(matrizes)
+    return performMatriz(matriz, movimentKit)
+}
+
+const desambiguateSingleBarCodeMultipleRegistries = async (ms: readonly Matriz[]): Promise<Matriz> => 
+    new Promise( async (resolve, reject) => {
+        const length = ms.length
+        if(length===1) {
+            // has one item
+            resolve(ms[0])
+        } else if(length>1) {
+            // has many items
+            const opts = ms.map( (m, index) => `${index+1}) PN=${m.partNumber} / MSG=${m.msg} (${m.printer} / @${m.remoteFieldId})`)
+            term.singleColumnMenu(opts, (err, response) => {
+                const index = response.selectedIndex
+                const choosed = ms[index]
+                resolve(choosed)
+            })
+        } else {
+            // FIX: We should show to user something like this : 'The nearest registry I found is this: ....'
+            // has no item
+            const err = 'A matriz lida nao esta cadastrada, o programa sera terminado.'
+            console.log(err)
+            await delay(5000)
+            reject(err)
+        }
+     })
+        
+    
 
 const main3 = () => {
-
+   
     const input = () => readKeyboardAsync()
         .tap( k => console.log(`${k.sequence}`) )
 
-    console.log('iniciando varredura de teclado...')
+    console.log('PROGRAMA INICIADO.')
+    console.log('Pronto para ler o barcode (voce também pode digitar manualmente o barcode a pressionar a tecla <Enter> em seguida)...')
 
     GetBarCodeFromSignal(input)
         .unsafeRun( maybeBarCode => {
@@ -24,8 +62,9 @@ const main3 = () => {
                 const partNumber = barCode.partNumber
                 console.log(`Iniciando realizacao do trabalho`)
                 makeMovimentKit()
-                    .then( movimentKit => {
-                        performMatrizByItsMsg(msg as unknown as any, movimentKit)
+                    .then( async movimentKit => {
+                        const barCodeRaw = barCode.raw.trim()
+                        await performMatrizByItsBarCode(barCodeRaw, movimentKit)
                     })  
             })
 
