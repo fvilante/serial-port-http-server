70b3d559a45361c1cae682590482ebf40c36665d
fvilante <fvilante1@gmail.com>
2021-05-14 15:32:55 -0300

    improved type safety

--- src/serial-port/adts/validated/kind.ts
+++ src/serial-port/adts/validated/kind.ts
@@ -4,7 +4,10 @@
 
 export type KindedWorld<K extends string, A> = ReturnType<Kinded<K,A>['unsafeRun']>
 
+export type InferKinded<T extends Kinded> = T extends Kinded<infer K, infer A> ? [K,A] : never
+
 export type Kinded<K extends string = string, A = unknown> = {
+    kind: 'Kinded'
     unsafeRun: () => [kind: K, value: A]
     //mapKind: <K1 extends string>(f: (_:K) => K1) => Kinded<K1,A>
     //mapValue: <B>(f: (_:A) => B) => Kinded<K,B>
@@ -17,6 +20,7 @@ export const Kinded = <K extends string, A>(world: () => KindedWorld<K,A>): Kind
     const unsafeRun: T['unsafeRun'] = world
     
     return {
-        unsafeRun
+        kind: 'Kinded',
+        unsafeRun,
     }
 }
