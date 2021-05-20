ffe089275bf09369ef9b70aa63f417eecde16792
fvilante <fvilante1@gmail.com>
2021-05-20 08:51:09 -0300

    improved Kind adt

--- src/serial-port/adts/validated/kind.test.ts
+++ src/serial-port/adts/validated/kind.test.ts
@@ -1,4 +1,4 @@
-import { Kinded } from "./kind"
+import { Kinded, Kinded_ } from "./kind"
 
 describe('Basic test', () => {
     
@@ -11,6 +11,35 @@ describe('Basic test', () => {
         // test
         const actual = action.unsafeRun()
         const expected = ["Channel", value]
+        expect(actual).toEqual(expected)
+    })
+
+    it('Can map a value', async () => {
+        // prepare
+        const value = 20
+        const Channel = (value: number) => Kinded<"Channel", number>(() => ['Channel', value])
+        // act
+        const ch = Channel(value)
+        const action = ch
+            .map( (kind, value) =>  [kind, `oi ${value}` as const] )
+        // test
+        const actual = action.unsafeRun()
+        const expected = ["Channel", "oi 20"]
+        expect(actual).toEqual(expected)
+    })
+
+    it('Can flatten a value', async () => {
+        // prepare
+        const value = 20
+        type Byte_ = ReturnType<typeof Byte_>
+        const Byte_ = (value: number) => Kinded(() => ['Byte_', value])
+        const Channel = (value: Byte_) => Kinded(() => ['Channel', value])
+        const probe = Channel(Byte_(value))
+        // act
+        const action = Kinded_.flatten(probe)
+        // test
+        const actual = action.unsafeRun()
+        const expected = ["Channel/Byte_", value]
         expect(actual).toEqual(expected)
     })
 })
--- src/serial-port/adts/validated/kind.ts
+++ src/serial-port/adts/validated/kind.ts
@@ -9,8 +9,12 @@ export type InferKinded<T extends Kinded> = T extends Kinded<infer K, infer A> ?
 export type Kinded<K extends string = string, A = unknown> = {
     kind: 'Kinded'
     unsafeRun: () => [kind: K, value: A]
-    //mapKind: <K1 extends string>(f: (_:K) => K1) => Kinded<K1,A>
-    //mapValue: <B>(f: (_:A) => B) => Kinded<K,B>
+    unsafeRunV: () => A
+    unsafeRunK: () => K
+    map: <K1 extends string, B>(f: (kind: K, value: A) => [kind: K1, value: B]) => Kinded<K1,B>
+    //fmap: <K1 extends string, B>(f: (kind: K, value: A) => Kinded) => Kinded<K1,B>
+    mapKind: <K1 extends string>(f: (_:K) => K1) => Kinded<K1,A>
+    mapValue: <B>(f: (_:A) => B) => Kinded<K,B>
 }
 
 export const Kinded = <K extends string, A>(world: () => KindedWorld<K,A>): Kinded<K,A> => {
@@ -18,9 +22,56 @@ export const Kinded = <K extends string, A>(world: () => KindedWorld<K,A>): Kind
     type T = Kinded<K,A>
 
     const unsafeRun: T['unsafeRun'] = world
+    const unsafeRunV: T['unsafeRunV'] = () => unsafeRun()[1]
+    const unsafeRunK: T['unsafeRunK'] = () => unsafeRun()[0]
+
+    const map: T['map'] = f => Kinded( () => {
+        const [kind, a] = unsafeRun()
+        const [newKind, b] = f(kind, a)
+        return [newKind, b]
+    })
+
+    const mapKind: T['mapKind'] = f => map( (kind, value) => {
+        const newKind = f(kind)
+        return [newKind, value]
+    })
+
+    const mapValue: T['mapValue'] = f => map( (kind, value) => {
+        const newValue = f(value)
+        return [kind, newValue]
+    })
     
     return {
         kind: 'Kinded',
         unsafeRun,
+        unsafeRunV,
+        unsafeRunK,
+        map,
+        mapKind,
+        mapValue,
     }
+}
+
+// static part
+
+export type Kinded_ = {
+    value: <K extends string, A>(kind: K, value: A) => Kinded<K,A>
+    flatten: <K1 extends string,K2 extends string,A>(mma: Kinded<K1, Kinded<K2, A>>) => Kinded<`${K1}/${K2}`, A>
+} 
+
+type T = Kinded_
+
+const value: T['value'] = (kind, value) => Kinded( () => [kind, value])
+
+const flatten: T['flatten'] = mma => Kinded( () => {
+    const [k1, ma] = mma.unsafeRun()
+    const [k2, a] = ma.unsafeRun()
+    const newK = `${k1}/${k2}` as const
+    return [newK,a]
+    
+})
+
+export const Kinded_ : Kinded_ = {
+    value,
+    flatten,
 }
