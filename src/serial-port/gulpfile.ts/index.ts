bf1a5bf896721b6ad2d4ad9e93a98c0f68009bcd
fvilante <fvilante1@gmail.com>
2021-06-02 10:44:34 -0300

    gulpFile now can spawn proccess

--- src/serial-port/gulpfile.ts/index.ts
+++ src/serial-port/gulpfile.ts/index.ts
@@ -1,27 +1,31 @@
 //import { series } from 'gulp'
 
 import { series, TaskFunction } from 'gulp'
+import { spawn } from 'child_process'
 
-
-// the 'clean' function is not exported so it can be considered a private task.
-// it can still be used with the 'series()' composition.
 
 const clean: TaskFunction = (done) => {
-    // body omitted
-    done()
+    const ls = spawn('dir')
+    ls.on('close', code => {
+        console.log('ok dir has runned', code)
+        done()
+    })
+    ls.stdout.on('data', data => {
+        console.log('data=', String(data))
+    })
+   
 }
 
-// the 'build' function is exported so it is public and can be run with the 'gulp' command.
-// it can also be used with the 'series()' composition.
 const build: TaskFunction = (done: any) => {
-    // body ommitted
+
     console.log("I'm build task")
     done()
 }
 
 const defaultTask: TaskFunction = (done: any) => {
-    //place code for your default task here
+
     console.log("I'm default task")
+    console.log("Executing datee")
     done() 
 }
 
@@ -30,4 +34,4 @@ const BuildClean: TaskFunction = series(defaultTask, build)
 // here you exports what you want to access through 'gulp-cli'
 // FIX: There is a way to use 'ESNEXT' normal export/import keywords, but I don't have time now to research this solution. Could be better to do that in future. (principe of less variation from standard)
 exports.build = build;
-exports.default = BuildClean;
+exports.default = clean;
