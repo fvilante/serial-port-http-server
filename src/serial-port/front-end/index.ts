5cf8cabbef7e68cab5e6968771f30349d8ab184a
fvilante <fvilante1@gmail.com>
2021-06-07 08:51:10 -0300

    added some more complex statements

--- src/serial-port/front-end/index.ts
+++ src/serial-port/front-end/index.ts
@@ -1,4 +1,33 @@
-import { Maybe, Maybe_ } from "../adts/maybe"
+//import { Maybe_ } from './maybe'
 
-type A = `Some text ${number}`
-export const getSomeText = (n: number):Maybe<A> => Maybe_.fromJust(`Some text ${n}`  as const)
+function program() {
+    const allDivs = document.querySelectorAll(".title");
+        
+    //const ma = Maybe_.fromJust('here is a maybe')
+
+    allDivs.forEach( div => {
+        let c = 0  
+        const newDiv = document.createElement('div')
+        newDiv.style.color = 'blue'
+        newDiv.innerText = `Button ref = ${c++}`
+        newDiv.addEventListener('click', (event) => {
+            //newDiv.style.cssText = 'color: green; background: black';
+            newDiv.innerHTML = `${event.clientX}, ${event.clientY} = ${c++} / ma=${2}`
+        })
+        newDiv.addEventListener('mouseenter', (event) => {
+            console.log('mouse entered at', event.clientY, event.clientX)
+            newDiv.style.cssText = 'color: while; background: cyan';
+        })
+        newDiv.addEventListener('mouseleave', (event) => {
+            newDiv.style.cssText = 'color: green; background: black';
+        })
+        div.appendChild(newDiv)
+        //div.innerHTML = 'juca'
+    }) 
+
+    
+
+}
+
+program();
+
