6814527a23de1d3dcc2fd828fe21f4bec4e93a3f
fvilante <fvilante1@gmail.com>
2021-06-24 14:05:54 -0300

    updated the concept of Ref Adt. Inspired in ZRef.

---
+++ src/serial-port/adts/ref/zref.test.ts
@@ -0,0 +1,12 @@
+import { ZRef_ } from "./zref"
+
+describe('basic tests', () => {
+
+    it('can construct from let', async () => {
+        //prepare
+        //act
+        //check
+        expect(true).toStrictEqual(true)
+    })
+
+})
