a444944ab959de0ea9a0eeb15f4143587c73a0f6
fvilante <fvilante1@gmail.com>
2021-11-05 15:09:59 -0300

    small change: clean up

--- src/software/cli-tools/le-toda-memoria.ts
+++ src/software/cli-tools/le-toda-memoria.ts
@@ -1,11 +1,11 @@
-import { BaudRate } from './serial/baudrate'
-import { FrameCore } from "./cmpp/datalink/cmpp-datalink-protocol";
-import { executeInSequence } from "./promise-utils";
-import { Address, Axis } from "./global-env/global";
-import { sendCmpp } from "./send-receive-cmpp-datalink";
-import { Range, groupBy, now } from './utils'
-import { delay } from "./utils/delay";
-import { bit_test } from "./bit-wise-utils";
+import { BaudRate } from '../serial/baudrate'
+import { FrameCore } from "../cmpp/datalink/cmpp-datalink-protocol";
+import { executeInSequence } from "../promise-utils";
+import { Address, Axis } from "../global-env/global";
+import { sendCmpp } from "../send-receive-cmpp-datalink";
+import { Range, groupBy, now } from '../utils'
+import { delay } from "../utils/delay";
+import { bit_test } from "../bit-wise-utils";
 
 
 
