5e611984e8365f62f3bcb93590c57e4a5e793eda
fvilante <fvilante1@gmail.com>
2021-11-08 11:17:25 -0300

    created unit test for 'listSerialports' file

--- src/software/serial/list-serial-ports.ts
+++ src/software/serial/list-serial-ports.ts
@@ -3,3 +3,31 @@ import { PortInfo } from "./types";
 
 
 export const listSerialPorts = (): Promise<readonly PortInfo[]> => SerialPort.list()
+
+
+// NOTE: 
+//  com0com is a free software to emulate a pair of loopback serial ports in microsoft-windows.
+//  below is a conventional output from a 'listSerialPorts' call:
+/*  
+    {
+        "locationId": "CNCA1",
+        "manufacturer": "Vyacheslav Frolov" as const,
+        "path": "COM4",                           
+        "pnpId": "COM0COM\\PORT\\CNCA1",
+        "productId": undefined,
+        "serialNumber": undefined,
+        "vendorId": undefined,
+    },
+    {
+        "locationId": "CNCB1",
+        "manufacturer": "Vyacheslav Frolov" as const,
+        "path": "COM5",                           
+        "pnpId": "COM0COM\\PORT\\CNCB1",
+        "productId": undefined,
+        "serialNumber": undefined,
+        "vendorId": undefined,
+    },
+*/
+export const isSerialPortEmulatedWithCom0Com = (port: PortInfo):boolean => {
+    return port.manufacturer === "Vyacheslav Frolov"
+}
