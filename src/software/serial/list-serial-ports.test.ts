a1f9d2e70b9b9d13ff955e8d24a6b6e3d0bbc278
fvilante <fvilante1@gmail.com>
2021-11-15 15:22:31 -0300

    minor change

--- src/software/serial/list-serial-ports.test.ts
+++ src/software/serial/list-serial-ports.test.ts
@@ -9,28 +9,6 @@ const Helper_ListPorts_or_Throw = async () => await listSerialPorts().fmap( r =>
 
 describe('list-serial-ports', () => {
     
-    //NOTE: com0com is a free software to emulate serial port.
-    //      it's not necessary to have this software instaled, but if you have this
-    //      test will check it. Maybe this test be removed in future, because it
-    //      may rise false alarm when other developers without com0com instaled
-    //      run the test in his machine.
-    //NOTE: The com0com is just a loopback at windows layer, see: http://com0com.sourceforge.net/
-    /*
-    
-    it('can list two serial ports emulated with com0com', async () => {
-
-        // prepare
-        const portsList = await Helper_ListPorts_or_Throw()
-        const onlyCom0ComEmulatedPorts = portsList.filter( port => isSerialPortEmulatedWithCom0Com(port))
-        const expected = 2 // expect two com0com emulated ports
-        // act
-        const actual = onlyCom0ComEmulatedPorts.length
-        // check
-        expect(actual).toEqual(expected);
-    })
-
-    */
-
     it('can list two loop-back emulated serial ports', async () => {
 
         // prepare
@@ -55,5 +33,27 @@ describe('list-serial-ports', () => {
             expect(true).toBe(false)
         }
     })
+
+    //NOTE: com0com is a free software to emulate serial port.
+    //      it's not necessary to have this software instaled, but if you have this
+    //      test will check it. Maybe this test be removed in future, because it
+    //      may rise false alarm when other developers without com0com instaled
+    //      run the test in his machine.
+    //NOTE: The com0com is just a loopback at windows layer, see: http://com0com.sourceforge.net/
+    /*
+    
+    it('can list two serial ports emulated with com0com', async () => {
+
+        // prepare
+        const portsList = await Helper_ListPorts_or_Throw()
+        const onlyCom0ComEmulatedPorts = portsList.filter( port => isSerialPortEmulatedWithCom0Com(port))
+        const expected = 2 // expect two com0com emulated ports
+        // act
+        const actual = onlyCom0ComEmulatedPorts.length
+        // check
+        expect(actual).toEqual(expected);
+    })
+
+    */
 
 })
