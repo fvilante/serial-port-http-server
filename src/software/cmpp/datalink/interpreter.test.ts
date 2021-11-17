618b72d7a312ece5c765ed4825e549253b4a07fc
fvilante <fvilante1@gmail.com>
2021-11-17 16:01:30 -0300

    added unit test for cmpp interpreter

---
+++ src/software/cmpp/datalink/interpreter.test.ts
@@ -0,0 +1,114 @@
+import { FrameInterpreted, InterpretIncomming } from "."
+import { ACK, ESC, ETX, NACK, STX } from "./core-types"
+
+const correctMasterFrame = [0x1B, 0x02, 0x01, 0x87, 0x3E, 0x3F, 0x1B, 0x03, 0xF6,]
+const correctMasterFrameInterpreted: FrameInterpreted = {
+    firstEsc: [ESC],
+    startByte: [STX],
+    dirChan: [0x01],
+    waddr: [0x87],
+    dataLow: [0x3E],
+    dataHigh: [0x3F],
+    lastEsc: [ESC],
+    etx: [ETX],
+    checkSum: [0xF6],
+    expectedChecksum: 0xF6,
+}
+
+const correctSlaveFrame = [0x1B, 0x06, 0x01, 0x87, 0x01, 0x86, 0x1B, 0x03, 0xE8,]
+const correctSlaveFrameInterpreted: FrameInterpreted = {
+    firstEsc: [ESC],
+    startByte: [ACK],
+    dirChan: [0x01],
+    waddr: [0x87],
+    dataLow: [0x01],
+    dataHigh: [0x86],
+    lastEsc: [ESC],
+    etx: [ETX],
+    checkSum: [0xE8],
+    expectedChecksum: 0xE8,
+}
+
+describe('basic tests', () => {
+
+    it('can parse a correct master frame', async () => {
+        //prepare
+        const probe = correctMasterFrame
+        const probeInterpreted = correctMasterFrameInterpreted
+        const parser = InterpretIncomming(ESC, [STX,ACK,NACK], ETX)
+        const parse = parser(
+            //frameinterpreted
+            (frameInterpreted, rawInput) => {
+                //check
+                expect(frameInterpreted).toStrictEqual(probeInterpreted)
+                expect(rawInput).toStrictEqual(probe)
+            },
+            //onError
+            (msg, partialFrame, rawInput) => {
+
+            },
+            //onStateChange
+            (currentState, partialFrame, waitingEscDup) => {
+                
+            }
+        )
+
+        //act
+        probe.forEach( byte => parse(byte))
+    })
+
+    it('can parse three correct master frames', async () => {
+        //prepare
+        let count = 0
+        const probe = [...correctMasterFrame,...correctMasterFrame, ...correctMasterFrame]
+        const probeInterpreted = correctMasterFrameInterpreted
+        const parser = InterpretIncomming(ESC, [STX,ACK,NACK], ETX)
+        const parse = parser(
+            //frameinterpreted
+            (frameInterpreted, rawInput) => {
+                //check
+                expect(frameInterpreted).toStrictEqual(correctMasterFrameInterpreted)
+                count++
+            },
+            //onError
+            (msg, partialFrame, rawInput) => {
+
+            },
+            //onStateChange
+            (currentState, partialFrame, waitingEscDup) => {
+                
+            }
+        )
+
+        //act
+        probe.forEach( byte => parse(byte))
+        expect(count).toEqual(3) 
+            
+    })
+
+    it('can parse a correct slave frame', async () => {
+        //prepare
+        const probe = correctSlaveFrame
+        const probeInterpreted = correctSlaveFrameInterpreted
+        const parser = InterpretIncomming(ESC, [STX,ACK,NACK], ETX)
+        const parse = parser(
+            //frameinterpreted
+            (frameInterpreted, rawInput) => {
+                //check
+                expect(frameInterpreted).toStrictEqual(probeInterpreted)
+                expect(rawInput).toStrictEqual(probe)
+            },
+            //onError
+            (msg, partialFrame, rawInput) => {
+
+            },
+            //onStateChange
+            (currentState, partialFrame, waitingEscDup) => {
+                
+            }
+        )
+
+        //act
+        probe.forEach( byte => parse(byte))
+    })
+})
