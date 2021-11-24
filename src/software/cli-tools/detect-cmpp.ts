836ab91df0bc4ad78b7258c6f645257e3b3ffacd
fvilante <fvilante1@gmail.com>
2021-11-24 20:16:40 -0300

    recoded the 'detect-cmpp' utility tool

--- src/software/cli-tools/detect-cmpp.ts
+++ src/software/cli-tools/detect-cmpp.ts
@@ -1,73 +1,105 @@
-import { BaudRate } from '../serial/baudrate'
-import { FrameCore, FrameInterpreted } from "../cmpp/datalink/index"
-import { listSerialPorts } from "../serial/index"
-import { ExecuteInParalel } from "../core/promise-utils"
-import { sendCmpp } from "../cmpp/datalink/send-receive-cmpp-datalink"
+import { listSerialPorts, PortOpener } from "../serial";
+import { frameCoreToPayload } from "../cmpp/datalink/frame";
+import { cmppSimpleTransaction } from "../cmpp/datalink/transactioners/simple-transaction";
+import { isSerialPortEmulatedWithCom0Com, isSerialPortLoopBackForTest } from "../serial/list-serial-ports";
+import { executeInSequence } from "../core/promise-utils";
+import { BaudRate } from "../serial/baudrate";
 
+//TODO: For some reason when CMPP is 'NOT DETECTED' some resource got not cleanned correctly. Solve this problem.
+//      Eventually should be better to change de algorithm to a more phase controled step-by-step 
+//      (ie: first open all different ports in a particular baudrate, proccess then in sequence, close, re-open in another baudrate until the end) 
 
+type DetectionResult = 'Detected' | 'Not Detected'
 
-type SerialChannel = {portName: string, baudRate: BaudRate}
-type CMPPDetectionResponse = Map<SerialChannel, {
-    type: 'CMPP - Posijet Mortor Driver' | 'CMPP NOT present'
-    detail: FrameInterpreted | string
-}>
-
-const detectCMPP = async ():Promise<CMPPDetectionResponse> => {
-
-    const res: CMPPDetectionResponse = new Map()
-    console.log(`Detectando portas seriais...`)
-    const portsInfo = await listSerialPorts()
-    const ports = portsInfo.map( x => x.path) as readonly string[]
-    console.log(`Portas detectadas:`, ports)
-    const baudRates: readonly BaudRate[] = [9600]
-    console.log(`As tentativas serao feitas feita em nestas velocidades (bps):`, baudRates)
-    console.log('Aguarde detectando...')
-    const DetectAll = ports.map( portName => {
-        // Fix: I would like to look for more speeds like 2400 etc and mult channel
-        const baudRate = 9600
-        const channel = 0
-        const waddr = 0xA0
-        const frame = FrameCore('STX','Solicitacao',channel, waddr,0)
-        return  async () => {
-            console.log(`Procurando na porta/baudrate = ${portName}/${baudRate}`)
-            return sendCmpp(portName, baudRate)(frame)
-            .then( frameInterpreted => {
-                console.log(`Encontrado na porta: ${portName}/${baudRate}`)
-                res.set({portName, baudRate},{
-                    type: 'CMPP - Posijet Mortor Driver',
-                    detail: frameInterpreted,
-                })
+// NOTE: the result promise never rejects, always resolve
+const detectCmppInPort = async (portPath: string, baudRate: BaudRate, timeout: number): Promise<DetectionResult> => {
+    const [payload] = frameCoreToPayload({
+        startByte: 'STX',
+        direction: 'Solicitacao',
+        waddr: 0x00,
+        channel: 0,
+        uint16: 0x00,
+    })
+    return new Promise( (resolve_, reject) => {
+        let id = setTimeout( () => {
+            resolve_('Not Detected')
+        }, timeout)
+        const resolve = (value:DetectionResult):void => {
+            clearTimeout(id)
+            resolve_(value)
+        }
+        PortOpener(portPath,baudRate)
+            .then( portOpened => {
+                cmppSimpleTransaction(portOpened)(payload)
+                    .then( frameInterpreted => {
+                        portOpened.close()
+                            .then( () => {
+                                resolve('Detected')
+                            })
+                            .catch( err => {
+                                resolve('Detected')
+                            })
+                        
+                    })
+                    .catch( err => {
+                        portOpened.close()
+                            .then( () => {
+                                resolve('Not Detected')
+                            })
+                            .catch( err => {
+                                resolve('Not Detected')
+                            })
+                    })
             })
             .catch( err => {
-                console.log(`**NÃO** encontrado na porta: ${portName}/${baudRate}`)
-                res.set({portName, baudRate},{
-                    type: 'CMPP NOT present',
-                    detail: `${err}`
-                })
+                resolve('Not Detected')
             })
-        }
     })
-    await ExecuteInParalel(DetectAll)
-        .then( () => {
-            // FIX: This report is not being show I do not found why
-            //      but console.log of low level functions are show the detection phase (provisorily)
-            console.log('Fim da busca.')
-            console.log("Produzindo report final")
-            res.forEach( (result, channel) => {
-                console.log(result, channel)
-            })
 
-        })
-
-    return res
-
-}
-
-
-const run = async ():Promise<void> => {
-
-    const printers = await detectCMPP()
     
 }
 
-run()
+
+//TODO: Extract this function to a util lib
+export const crossProduct = <A,B>(as: readonly A[], bs: readonly B[]): readonly (readonly [A,B])[] => {
+    let buffer: readonly (readonly [A,B])[] = []
+    as.forEach( a => {
+        bs.forEach( b => {
+            buffer = [...buffer, [a,b]]
+        })
+    })
+    return buffer
+}
+
+const main = async () => {
+    const timeout = 400 //TODO: improve this number
+    const baudRates: readonly BaudRate[] = [9600]
+    const ports_ = await listSerialPorts()
+    const ports = ports_.filter( portInfo => {
+        const isPortEmulated = isSerialPortEmulatedWithCom0Com(portInfo) || isSerialPortLoopBackForTest(portInfo)
+        return !isPortEmulated
+    })
+    const [...toCheck] = crossProduct(ports, baudRates)
+    const effects = toCheck.map( probe => {
+        return () => new Promise<void>( (resolve, reject) => {
+            const [portToCheck, baudRate] = probe
+            const path = portToCheck.path
+            //console.log(`verificando em: ${path}/${baudRate}`)
+            detectCmppInPort(path, baudRate, timeout)
+                .then( result => {
+                    console.log(`Resultado em: ${path}/${baudRate}  ===> ${result}`)
+                    resolve()
+                })
+            })
+            
+    })
+
+    await executeInSequence(effects)
+        
+     
+
+}
+
+main().then( () => {
+    console.log('fim')
+});
