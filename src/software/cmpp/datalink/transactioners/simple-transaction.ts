4265f47b2af74bab211ebfd1403f6647a99c8bb9
fvilante <fvilante1@gmail.com>
2021-11-26 17:03:20 -0300

    improved cleaningup of resources

--- src/software/cmpp/datalink/transactioners/simple-transaction.ts
+++ src/software/cmpp/datalink/transactioners/simple-transaction.ts
@@ -3,34 +3,54 @@ import { PortOpened } from "../../../serial/port-opener";
 import { StartByteNum, STX } from "../core-types";
 import { Payload, makeWellFormedFrame } from "../payload";
 import { CmppDataLinkInterpreter } from "../interpreter";
+import { runOnce } from "../../../core/utils";
 
 //NOTE: if error on interpretation the promisse throws ErrorEvent
 //NOTE: This is the most simple form for a transaction with cmpp, there are other more efficient way to transact frames
 //NOTE: This function WILL NOT automatically close the port
 //TODO: create a function like that but that will attempt to retransmit failed transmission N times before effectivelly fail
 export const cmppSimpleTransaction = (portOpened: PortOpened) => (payload: Payload, startByte: StartByteNum = STX): Promise<FrameInterpreted> => {
+    
+    const cleanupPortResources = runOnce( () => {
+        //TODO: Encapsulates this cleanup in a more safe form. Refactor extract to a new object
+        try {
+            //NOTE: This try block is because looback emulated serial port do not have this __unsafe method implemented
+            const p = portOpened.__unsafeGetConcreteDriver() //TODO: avoid to use this __unsafe method
+            p.removeAllListeners('data')
+        } catch (err) {
+            //do nothing
+        }   
+    })
+    
     return new Promise( (resolve, reject) => {
         //TODO: Should this code be in a try..catch clause ?
         const parse = CmppDataLinkInterpreter({
             onSuccess: event => {
+                cleanupPortResources()
                 const { frameInterpreted } = event
                 resolve(frameInterpreted)
             },
             onError: event => {
+                cleanupPortResources()
                 reject(event)
             }
         })
 
-        //prepare port reception-handler
-        portOpened.onData( data => {
+        const receptionHandler = (data: readonly number[]) => {
             data.forEach( byte => {
                 const reset = parse(byte)
             })
-        })
-        // write data
-        const dataSerialized = makeWellFormedFrame(startByte,payload)
-        portOpened.write(dataSerialized) //TODO: Make code to wait this write signal return the promise !!
+        }
 
+        // make frame
+        const dataSerialized = makeWellFormedFrame(startByte,payload)
+        
+        // run: ---
+
+        // write data
+        //set reception handler
+        portOpened.onData(receptionHandler) //TODO: Loopback serial port emulated (used in unit tests) requires this handler to be set before transmission, but in real hardware this is not a assumption. Refactor so tests works as real hardware.
+        portOpened.write(dataSerialized)
         
     })
     
