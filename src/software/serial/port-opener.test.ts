2503ceea0b3d6d832712054e1b679a8c522c156e
fvilante <fvilante1@gmail.com>
2021-11-26 15:54:33 -0300

    minor change

--- src/software/serial/port-opener.test.ts
+++ src/software/serial/port-opener.test.ts
@@ -1,12 +1,8 @@
-import { PortOpener } from './port-opener'
-
-//TODO: This file should be rename 'loopback.test.ts' because it is in fact testing the loopback facility
 
 describe('Basic tests', () => {
 
     it('cannot open a port that is not reachable using portsList', async () => {
-
-        const port = await PortOpener('com50',9600)
+        //TO BE IMPLEMENTED
         expect(true).toEqual(true);
 
     });
