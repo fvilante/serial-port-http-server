e599dee2ea5f18f2e250fdf24b7d477e2bc12352
fvilante <fvilante1@gmail.com>
2021-11-27 08:53:09 -0300

    Validated ADT introduced

---
+++ src/software/adts/maybe/validated.test.ts
@@ -0,0 +1,10 @@
+
+//TODO: to be done, experiment copy maybe test. Validated ADT was done basically over the 
+//      current code of Maybe ADT
+describe('Basic Tests', () => {
+
+    it('xxx', async () => {
+        //TODO: to be implemented
+        expect(true).toEqual(true)
+    })
+})
