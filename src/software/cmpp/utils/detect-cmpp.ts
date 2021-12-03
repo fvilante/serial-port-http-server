17eab872b8d82f816da8636a8863cbe7abc4d314
fvilante <fvilante1@gmail.com>
2021-12-03 11:38:56 -0300

    1. extracted 'closePort' function, 2. retry logic as reentrant function

--- src/software/cmpp/utils/detect-cmpp.ts
+++ src/software/cmpp/utils/detect-cmpp.ts
@@ -2,7 +2,7 @@ import { PortOpened, PortSpec } from "../../serial"
 import { PortOpenError, portOpener_CB } from "../../serial/port-opener-cb"
 import { Channel } from "../datalink/core-types"
 import { frameCoreToPayload } from "../datalink/frame-core"
-import { payloadTransact } from "../datalink/transactioners/payload-transact"
+import { payloadTransact, PayloadTransactError } from "../datalink/transactioners/payload-transact"
 
 //NOTE: 'true' means cmpp is present in that channel/port, 'false' otherwise.
 // NOTE: This payload is just a information request of any arbitrary cmpp address
@@ -34,9 +34,34 @@ export type EventHandler = {
 
 //NOTE: you must call this function when there is no more then one cmpp per each Tunnel connected
 //NOTE: Important! this function never throws
-export const detectCmpp = (tunnel: Tunnel, timeoutMilisecs: number, handler: EventHandler): void => {
+export const detectCmpp = (tunnel: Tunnel, timeoutMilisecs: number, handler: EventHandler, retryCounter: number = 3): void => {
 
     const { channel, portSpec} = tunnel
+
+    const closePortSafe = (portOpened: PortOpened): Promise<void> => {
+        return new Promise( (resolve, reject) => {
+            //close port
+            try {
+                portOpened.close()
+                    .then( () => {
+                        // ok sucessful closed
+                        resolve()
+                    })
+                    .catch( err => {
+                        handler?.onError(err)
+                    })
+                    .finally( () => {
+                        handler?.END()
+                        resolve()
+                    })
+            } catch (err) {
+                handler?.onError(err)
+                handler?.END()
+                resolve()
+            }
+        })
+        
+    }
 
     portOpener_CB(portSpec, {
         onError: err => {
@@ -52,25 +77,7 @@ export const detectCmpp = (tunnel: Tunnel, timeoutMilisecs: number, handler: Eve
                     } else {
                         handler?.onNotDetected(tunnel)
                     }
-                    //close port
-                    try {
-                        portOpened.close()
-                            .then( () => {
-                                // ok sucessful closed
-                                resolve()
-                            })
-                            .catch( err => {
-                                handler?.onError(err)
-                            })
-                            .finally( () => {
-                                handler?.END()
-                                resolve()
-                            })
-                    } catch (err) {
-                        handler?.onError(err)
-                        handler?.END()
-                        resolve()
-                    }
+                    closePortSafe(portOpened).then( () => resolve())
                 }) 
             }
 
@@ -83,8 +90,24 @@ export const detectCmpp = (tunnel: Tunnel, timeoutMilisecs: number, handler: Eve
                     cleanupAndFinalize(true, tunnel)
                 })
                 .catch( err => {
-                    handler?.onError(err)
-                    cleanupAndFinalize(false, tunnel)
+                    const err_: PayloadTransactError = err as PayloadTransactError
+                    const [transactError ] = err_
+                    if(transactError.kind === 'ErrorEvent' ) {
+                        //is not timout error but 'interpretation?' error, then:
+                        //try again!
+                        closePortSafe(portOpened)
+                            .then( () => {
+                                if (retryCounter>0) {
+                                    detectCmpp(tunnel,timeoutMilisecs,handler,retryCounter-1)
+                                }
+                            })
+                    } else {
+                        //console.table(err)
+                        handler?.onError(err)
+                        cleanupAndFinalize(false, tunnel)
+                    }
+
+                    
                 })
             } catch (err) {
                 handler?.onError(err)
