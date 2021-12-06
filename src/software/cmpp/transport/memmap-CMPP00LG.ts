62237572875cd7e3e2f57ff68b5dfa9bd97a28d3
fvilante <fvilante1@gmail.com>
2021-12-06 14:21:49 -0300

    added a safe type to all parameters in memmap caster

--- src/software/cmpp/transport/memmap-CMPP00LG.ts
+++ src/software/cmpp/transport/memmap-CMPP00LG.ts
@@ -1,96 +1,122 @@
-import { param_16bits, param_1bit, param_8bits } from './memmap-core'
+import { param_16bits, param_1bit, param_8bits, UInt16 } from './memmap-core'
 
 // typing casting
 
 const X: number = 0xA0 // fix: confirmar endereço
+
+const id = <A extends number>(x: A):A => x
+
+//types
+const identityType = {
+    serialize: id,
+    deserialize: id, 
+}
+
 
 // ---- Inicio dos dados do usuario ---------
 
 const PosicaoInicial = param_16bits({
     name: 'Posicao inicial',
     waddr: X+0x00,
+    ...identityType,
 })
 
 const PosicaoFinal = param_16bits({
     name: 'Posicao final',
     waddr: X+0x02,
+    ...identityType,
 })
 
 const AceleracaoDeAvanco = param_16bits({
     name: 'Aceleracao de avanco',
     waddr: X+0x04,
+    ...identityType,
 })
 
 const AceleracaoDeRetorno = param_16bits({
     name: 'Aceleracao de retorno',
     waddr: X+0x06,
+    ...identityType,
 })
 
 const VelocidadeDeAvanco = param_16bits({
     name: 'Velocidade de avanco',
     waddr: X+0x08,
+    ...identityType,
 })
 
 const VelocidadeDeRetorno = param_16bits({
     name: 'Velocidade de retorno',
     waddr: X+0x0A,
+    ...identityType,
 })
 
 const NumeroDeMensagemNoAvanco = param_8bits({
     name: 'Numero de mensagem no avanco',
     waddr: X+0xC,
     startBit: 0
+    ...identityType,
 })
 
 const NumeroDeMensagemNoRetorno = param_8bits({
     name: 'Numero de mensagem no retorno',
     waddr: X+0xC,
     startBit: 8,
+    ...identityType,
 })
 
 const PosicaoDaPrimeiraImpressaoNoAvanco = param_16bits({
     name: 'Posicao da primeira impressao no avanco',
     waddr: X+0x0E,
+    ...identityType,
 })
 
 const PosicaoDaPrimeiraImpressaoNoRetorno = param_16bits({
     name: 'Posicao da primeira impressao no retorno',
     waddr: X+0x10,
+    ...identityType,
 })
 
 const PosicaoDaUltimaMensagemNoAvanco = param_16bits({
     name: 'Posicao da ultima mensagem no avanco',
     waddr: X+0x12,
+    ...identityType,
 })
 
 const PosicaoDaUltimaMensagemNoRetorno = param_16bits({
     name: 'Posicao da ultima mensagem no retorno',
     waddr: X+0x14,
+    ...identityType,
 })
 
 const LarguraDoSinalDeImpressao = param_16bits({
     name: 'Largura do sinal de impressao',
     waddr: X+0x16,
+    ...identityType,
 })
 
 const TempoParaStartAutomatico = param_16bits({
     name: 'Tempo para o start automatico',
     waddr: X+0x18,
+    ...identityType,
 })
 
 const TempoParaStartExterno = param_16bits({
     name: 'Tempo para o start externo',
     waddr: X+0x1A,
+    ...identityType,
 })
 
 const CotaDeAntecipacaoDoStartEntreEixosPinelmatico = param_16bits({
     name: 'Cota de antecipacao do start entre eixos (pinelmatico)',
     waddr: X+0x1C,
+    ...identityType,
 })
 
 const RetardoParaStartAutomaticoPassoAPasso = param_16bits({
     name: 'Retardo para o start automatico passo a passo',
     waddr: X+0x1E,
+    ...identityType,
 })
 
 // X+0x20 = Flag de configuracao da programacao
@@ -99,42 +125,49 @@ const StartAutomaticoNoAvancoLigado = param_1bit({
     name: 'Start automatico no avanco ligado',
     waddr: X+0x20,
     startBit: 0,
+    ...identityType,
 })
 
 const StartAutomaticoNoRetornoLigado = param_1bit({
     name: 'Saida de start no avanco ligado',
     waddr: X+0x20,
     startBit: 2,
+    ...identityType,
 })
 
 const SaidaDeStartNoRetornoLigado = param_1bit({
     name: 'Saida de start no retorno ligado',
     waddr: X+0x20,
     startBit: 3,
+    ...identityType,
 })
 
 const StartExternoHabilitado = param_1bit({
     name: 'Start externo habilitado',
     waddr: X+0x20,
     startBit: 4,
+    ...identityType,
 })
 
 const LogicaDoStartExterno = param_1bit({
     name: 'Logica do start externo',
     waddr: X+0x20,
     startBit: 5,
+    ...identityType,
 })
 
 const EntradaDeStartEntreEixoHabilitado = param_1bit({
     name: 'Entrada de start entre eixo habilitado',
     waddr: X+0x20,
     startBit: 6,
+    ...identityType,
 })
 
 const StartExternoParaReferenciarHabilitado = param_1bit({
     name: 'Start externo para referenciar habilitado',
     waddr: X+0x20,
     startBit: 7,
+    ...identityType,
 })
 
 // X+0x21 = Flag de configuracao da programacao
@@ -143,48 +176,56 @@ const LogicaDoSinalDeImpressao = param_1bit({
     name: 'Logica do sinal de impressao',
     waddr: X+0x20,
     startBit: 8,
+    ...identityType,
 })
 
 const LogicaDoSinalDeReversao = param_1bit({
     name: 'Logica do sinal de reversao',
     waddr: X+0x20,
     startBit: 9,
+    ...identityType,
 })
 
 const SelecaoDeImpressaoViaSerialLigada = param_1bit({
     name: 'Selecao de impressao via serial ligada',
     waddr: X+0x20,
     startBit: 10,
+    ...identityType,
 })
     
 const ReversaoDeImpressaoViaSerialLigada = param_1bit({
     name: 'Reversao de impressao via serial ligada',
     waddr: X+0x20,
     startBit: 11,
+    ...identityType,
 })
 
 const ZeroIndexHabilitadoParaProtecao = param_1bit({
     name: 'Zero Index habilitado p/ protecao',
     waddr: X+0x20,
     startBit: 12,
+    ...identityType,
 })
 
 const ZeroIndexHabilitadoParaCorrecao = param_1bit({
     name: 'Zero Index habilitado p/ correcao',
     waddr: X+0x20,
     startBit: 13,
+    ...identityType,
 })
 
 const ReducaoDoNivelDeCorrenteEmRepouso = param_1bit({
     name: 'Reducao do nivel de corrente em repouso',
     waddr: X+0x20,
     startBit: 14,
+    ...identityType,
 })
 
 const ModoContinuoOuPassoAPasso = param_1bit({
     name: 'Modo continuo/passo a passo',
     waddr: X+0x20,
     startBit: 15,
+    ...identityType,
 })
 
 // ---------------
@@ -192,6 +233,7 @@ const ModoContinuoOuPassoAPasso = param_1bit({
 const RetardoParaOSinalDeImpressao = param_16bits({
     name: 'Retardo para o sinal de impressao',
     waddr: X+0x22,
+    ...identityType,
 })
 
 /*
@@ -209,26 +251,31 @@ const Vago = param_16bits({
 const ToleranciaDeErroDoZeroIndex = param_16bits({
     name: 'Tolerancia de Erro do zero index',
     waddr: X+0x26,
+    ...identityType,
 })
 
 const NumeroDePulsosPorVoltaDoMotor = param_16bits({
     name: 'Numero de pulsos por volta do motor',
     waddr: X+0x28,
+    ...identityType,
 })
 
 const ValorProgramadoDaReferencia = param_16bits({
     name: 'Valor programado da referencia',
     waddr: X+0x2A,
+    ...identityType,
 })
 
 const AceleracaoDeReferencia = param_16bits({
     name: 'Aceleracao de referencia',
     waddr: X+0x2C,
+    ...identityType,
 })
 
 const VelocidadeDeReferencia = param_16bits({
     name: 'Velocidade de referencia',
     waddr: X+0x2A, //2E
+    ...identityType,
 })
 
 // X+0x30 = Flag especial de intertravamento
@@ -237,36 +284,42 @@ const SaidaDeStartPassoAPasso = param_1bit({
     name: 'Saida de start passo a passo',
     waddr: X+0x30,
     startBit: 0,
+    ...identityType,
 })
 
 const StartAutomaticoPassoAPasso = param_1bit({
     name: 'Start automatico passo a passo',
     waddr: X+0x30,
     startBit: 1,
+    ...identityType,
 })
 
 const SelecaoDeMensagemPorMultipla = param_1bit({
     name: 'Selecao de mensagem por multipla',
     waddr: X+0x30,
     startBit: 2,
+    ...identityType,
 })
 
 const SelecaoDeMensagemPorImpressao = param_1bit({
     name: 'Selecao de mensagem por impresao',
     waddr: X+0x30,
     startBit: 3,
+    ...identityType,
 })
 
 const SelecaoDePensagemPelaParalela = param_1bit({
     name: 'Selecao de mensagem pela paralela',
     waddr: X+0x30,
     startBit: 4,
+    ...identityType,
 })
 
-const SelecaoDeMensagemDecrementadoNoRetorno = ({
+const SelecaoDeMensagemDecrementadoNoRetorno = param_1bit({
     name: 'Selecao de mensagem Decrementado no retorno',
     waddr: X+0x30,
     startBit: 5,
+    ...identityType,
 })
 
 // ------
@@ -275,6 +328,7 @@ const SelecaoDeMensagemDecrementadoNoRetorno = ({
 const DivisorProgramadoDomotor = param_16bits({
     name: 'Divisor programado do motor',
     waddr: X+0x31,
+    ...identityType,
 })
 */
 
@@ -286,42 +340,49 @@ const StartSerial = param_1bit({
     name: 'Start serial',
     waddr: X+0x32,
     startBit: 0,
+    ...identityType,
 })
 
 const StopSerial = param_1bit({
     name: 'Stop serial',
     waddr: X+0x32,
     startBit: 1,
+    ...identityType,
 })
 
 const PausaSerial = param_1bit({
     name: 'Pausa serial',
     waddr: X+0x32,
     startBit: 2,
+    ...identityType,
 })
 
 const MonoManualSerial = param_1bit({
     name: 'Modo manual serial',
     waddr: X+0x32,
     startBit: 3,
+    ...identityType,
 })
 
 const TesteDeImpressaoSerial = param_1bit({
     name: 'Teste de impressao serial',
     waddr: X+0x32,
     startBit: 4,
+    ...identityType,
 })
 
 const UsadoNaBahiaSulDescritoNaRotinaLEITOR_OK = param_1bit({
     name: 'Usado na bahia sul, descrito na rotina LEITOK',
     waddr: X+0x32,
     startBit: 5,
+    ...identityType,
 })
 
 const GravaEprom2 = param_1bit({
     name: 'Grava eprom2',
     waddr: X+0x32,
     startBit: 6,
+    ...identityType,
 })
 
 /*
@@ -329,6 +390,7 @@ const vago = param_1bit({
     name: 'vago',
     waddr: X+0x32,
     startBit: 7,
+    ...identityType,
 })*/
 
 // ----------
@@ -339,12 +401,14 @@ const vago = param_1bit({
 const DiferencaEntreSaidaDoFCMenosEOPrimeiroGiroDoZeroIndex = param_16bits({
     name: 'Diferenca entre saida do FC- e o primeiro giro do zindex',
     waddr: X+0x34,
+    ...identityType,
 })
 
 const ValorAnteriorDaPortaC = param_1bit({
     name: 'Valor anterior da porta C',
     waddr: X+0x36,
     startBit: 0,
+    ...identityType,
 })
 
 // 0x37 = Flag de uso geral
@@ -353,24 +417,28 @@ const FinalizacaoDaReferencia = param_1bit({
     name: 'Finalizacao da referencia',
     waddr: X+0x36,
     startBit: 8,
+    ...identityType,
 })
 
 const BitDeValorDoZeroIndexInvalido = param_1bit({
     name: 'Bit de valor do zero index invalido',
     waddr: X+0x36,
     startBit: 9,
+    ...identityType,
 })
 
 const StartAutomaticoPendente = param_1bit({
     name: 'Start automatico pendente',
     waddr: X+0x36,
     startBit: 10,
+    ...identityType,
 })
 
 const StartEntreEixosPendente = param_1bit({
     name: 'Start entre eixo pendente',
     waddr: X+0x36,
     startBit: 11,
+    ...identityType,
 })
 
 //;D3	Solicitacao de reversao de mensagem via serial
@@ -380,18 +448,21 @@ const AcessoAEpromViaSerial = param_1bit({
     name: 'Acesso a eprom via serial',
     waddr: X+0x36,
     startBit: 13,
+    ...identityType,
 })
     
 const GravacaoDeBlocoNaEprom2EmAndamento = param_1bit({
     name: 'Gravacao de bloco na eprom2 em andamento',
     waddr: X+0x36,
     startBit: 14,
+    ...identityType,
 })
 
 const GravacaoDaEprom2EmAndamento =  param_1bit({
     name: 'Gravacao da eprom2 em andamento',
     waddr: X+0x36,
     startBit: 15,
+    ...identityType,
 })
 
 //0x39 = Nivel dos sinais de fc-/fc+/ref/zindex
@@ -401,48 +472,56 @@ const NivelSinalHalfFull = param_1bit({
     name: 'Nivel: H/F',
     waddr: X+0x38,
     startBit: 8,
+    ...identityType,
 })
 
 const NivelSinalNmotor = param_1bit({
     name: 'Nivel: Nmotor',
     waddr: X+0x38,
     startBit: 9,
+    ...identityType,
 })
 
 const NivelSinalGiro = param_1bit({
     name: 'Nivel: FC+', // NOTE: Sinal de giro também é chamado de FC+ por questao de legado
     waddr: X+0x38,
     startBit: 10,
+    ...identityType,
 })
 
 const NivelSinalDmotor = param_1bit({
     name: 'Nivel: Dmotor',
     waddr: X+0x38,
     startBit: 11,
+    ...identityType,
 })
 
 const NivelSinalCKmotor = param_1bit({
     name: 'Nivel: CKmotor',
     waddr: X+0x38,
     startBit: 12,
+    ...identityType,
 })
 
 const NivelSinalFimDeCursoMenos = param_1bit({
     name: 'Nivel: FC-',
     waddr: X+0x38,
     startBit: 13,
+    ...identityType,
 })
 
 const NivelSinalReferencia = param_1bit({
     name: 'Nivel: REF',
     waddr: X+0x38,
     startBit: 14,
+    ...identityType,
 })
 
 const NivelSinalEmotor = param_1bit({
     name: 'Nivel: Emotor',
     waddr: X+0x38,
     startBit: 15,
+    ...identityType,
 })
 
 // api 
--- src/software/cmpp/transport/memmap-core.ts
+++ src/software/cmpp/transport/memmap-core.ts
@@ -6,43 +6,54 @@ import { Byte } from "../../core/byte"
 //TODO: Extract this type to a more convenient file (ie: core-types.ts ?!)
 export type StartBit = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 
 
-export type ParamCore<T extends string> = {
+export type UInt16 = number // unsigned integer of 16 bits //TODO: Improve the safety of this cast (ie: Validated<A> ?!)
+export type UInt8 = number // unsigned integer of 8 bits //TODO: Improve the safety of this cast (ie: Validated<A> ?!)
+export type UInt1 = number // unsigned integer of 1 bits //TODO: Improve the safety of this cast (ie: Validated<A> ?!)
+
+
+export type ParamCore<T extends string, A, B> = {
     name: T
     waddr: Byte   // word address 
+} & CmppType<A,B>
+
+// NOTE: 'A' is the source in user level, 'B' is the target in the transport level
+export type CmppType<A,B> = {
+    serialize: (_:A) => B
+    deserialize: (_:B) => A
 }
 
-export type Param_16bits<T extends string> = {
+export type Param_16bits<T extends string, A> = {
     type: '16 Bits'
-} & ParamCore<T>
+} & ParamCore<T,A, UInt16>
 
 
-export type Param_8bits<T extends string> = {
+export type Param_8bits<T extends string, A> = {
     type: '8 Bits'
     startBit: StartBit //takes 8 bits stating in 'startsBit' (included)
-} & ParamCore<T>
+} & ParamCore<T,A,UInt8>
 
 
-export type Param_1bit<T extends string> = { 
+export type Param_1bit<T extends string,A> = { 
     type: '1 Bit'
     startBit: StartBit //takes 1 bits stating in 'startsBit' (included)
-} & ParamCore<T>
+} & ParamCore<T,A,UInt1>
 
-export type Param<T extends string> = 
-    | Param_16bits<T>
-    | Param_8bits<T>
-    | Param_1bit<T>
+export type Param<T extends string, A> = 
+    | Param_16bits<T,A>
+    | Param_8bits<T,A>
+    | Param_1bit<T,A>
 
 
 // constructors
 
-export const param_16bits = <T extends string>(etc: Omit<Param_16bits<T>,'type'>):Param_16bits<T> => {
+export const param_16bits = <T extends string,A>(etc: Omit<Param_16bits<T,A>,'type'>):Param_16bits<T,A> => {
     return {  type: '16 Bits', ...etc }
 }
 
-export const param_8bits = <T extends string>(etc: Omit<Param_8bits<T>,'type'>):Param_8bits<T> => {
+export const param_8bits = <T extends string,A>(etc: Omit<Param_8bits<T,A>,'type'>):Param_8bits<T,A> => {
     return {  type: '8 Bits', ...etc }
 }
 
-export const param_1bit = <T extends string>(etc: Omit<Param_1bit<T>,'type'>):Param_1bit<T> => {
+export const param_1bit = <T extends string,A>(etc: Omit<Param_1bit<T,A>,'type'>):Param_1bit<T,A> => {
     return {  type: '1 Bit', ...etc }
 }
