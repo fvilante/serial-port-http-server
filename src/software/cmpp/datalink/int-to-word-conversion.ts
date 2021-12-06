a305b357a0086fe8d2f9f53c1c500e45f3fd136f
fvilante <fvilante1@gmail.com>
2021-12-06 17:47:05 -0300

    comment

--- src/software/cmpp/datalink/int-to-word-conversion.ts
+++ src/software/cmpp/datalink/int-to-word-conversion.ts
@@ -1,10 +1,13 @@
 
+
+//TODO: invert input to DataLow first then DataHigh, and use interface in function argument
 //TODO: rename to word2Uint16
 export function word2int(dadoH: number, dadoL: number): number {
     // fix: check if dadoH and dadoL is beetween 0 and 0xff
     return dadoH * 256 + dadoL 
 }
 
+//TODO: invert input to DataLow first then DataHigh, and use interface in function argument
 //TODO: rename to Uint16ToWord
 export function int2word(uint16: number): [dadoH: number, dadoL: number] {
     // fix: check if uint16 is beetween 0 and 0xffff
