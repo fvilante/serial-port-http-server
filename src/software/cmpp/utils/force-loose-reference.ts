1091804cbef6f577fa0c902c95f0f6eec84f7467
fvilante <fvilante1@gmail.com>
2021-12-07 15:49:27 -0300

    added feature to make cmpp forcely loose its reference

---
+++ src/software/cmpp/utils/force-loose-reference.ts
@@ -0,0 +1,38 @@
+import { BaudRate } from "../../serial/baudrate"
+import { Channel } from "../datalink/core-types"
+import { CMPP00LG } from "../transport/memmap-CMPP00LG"
+import { Tunnel } from "./detect-cmpp"
+import { getStatusLow } from "./get-status-low"
+import { decomposeTunnel, makeTunnel } from "./tunnel-core"
+
+const makeAxis_ = CMPP00LG
+
+
+
+
+export const isReferenced = async (tunnel: Tunnel, makeAxis: typeof makeAxis_): Promise<boolean> => {
+    const { path, channel, baudRate} = decomposeTunnel(tunnel)
+    const statusL = await getStatusLow(path, baudRate, channel)
+    return statusL.referenciado
+}
+
+export const forceLooseReference = async (tunnel: Tunnel, makeAxis: typeof makeAxis_): Promise<void> => {
+    const axis = makeAxis(tunnel)
+    await axis.set('Pausa serial','ligado')
+}
+
+const test1 = async () => {
+
+    const tunnel = makeTunnel('com50',9600,0)
+
+    const isRefBefore = await isReferenced(tunnel, makeAxis_)
+    await forceLooseReference(tunnel, makeAxis_)
+    const isRefAfter = await isReferenced(tunnel, makeAxis_)
+    console.table({
+        isRefBefore,
+        isRefAfter
+    })
+
+}
+
+//test1()
