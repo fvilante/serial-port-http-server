4973ebea9052023e50ce1f8b5018a96af02652fb
fvilante <fvilante1@gmail.com>
2021-12-07 16:31:48 -0300

    improvement: what reference to finish

--- src/software/cmpp/utils/do-reference.ts
+++ src/software/cmpp/utils/do-reference.ts
@@ -1,17 +1,46 @@
 import { CMPP00LG } from "../transport/memmap-CMPP00LG"
 import { Tunnel } from "./detect-cmpp"
 import { forceLooseReference } from "./force-loose-reference"
-import { makeTunnel } from "./core"
+import { decomposeTunnel, isReferenced, makeTunnel } from "./core"
+import { getStatusLow } from "./get-status-low"
+import { PulsesPerTick, PulsesPerTickSquared } from "../transport/memmap-types"
 
 const makeAxis_ = CMPP00LG
 
+//TODO: Extract from this file to better place if possible
+export const isReferencing = async (tunnel: Tunnel, makeAxis: typeof makeAxis_):Promise<boolean> => {
+    const { path, baudRate, channel} = decomposeTunnel(tunnel)
+    const statusL = await getStatusLow(path, baudRate, channel)
+    const isReferencing = statusL.referenciado===false && statusL.referenciando===true
+    return isReferencing
+} 
+
+
+//NOTE: only returns when axis finished the reference proccess
 export const doReference = async (tunnel: Tunnel, makeAxis: typeof makeAxis_): Promise<void> => {
+    
+    return new Promise ( async (resolve, reject) => {
 
-    const axis = makeAxis(tunnel)
+        const axis = makeAxis(tunnel)
 
-    await axis.set('Pausa serial','desligado')
-    await axis.set('Start serial','ligado')
+        await axis.set('Velocidade de referencia', PulsesPerTick(1000))
+        await axis.set('Aceleracao de referencia', PulsesPerTickSquared(5000))
 
+        // do
+        await axis.set('Pausa serial','desligado')
+        await axis.set('Start serial','ligado')
+
+
+
+        while( await isReferencing(tunnel, makeAxis) ) {
+            // wait until reference proccess is done!
+        }
+
+        resolve()
+    
+    })
+
+    
 }
 
 const test2 = async () => {
