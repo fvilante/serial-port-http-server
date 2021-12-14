6e2f2a9c2787516a9b3a8b3bc9b7afbb9ec9ce33
fvilante <fvilante1@gmail.com>
2021-12-14 18:05:14 -0300

    introduced web server with fastfy
    it is working for local host in windows 7, but not working for ipv4.
    
    i will attempt this solution: https://github.com/fastify/fastify/issues/1620#issuecomment-488077377

---
+++ src/software/front-end/server.ts
@@ -0,0 +1,27 @@
+
+import Fastify from 'fastify'
+//import { run } from '../cmpp/controlers/examples/example-07'
+
+const port = 8080
+
+// Instantiate the framework
+const fastify = Fastify({
+    logger: true
+})
+
+// Declare a route
+fastify.get('/', async (request, reply) => {
+    //await run() 
+    return { hello: 'world' }
+})
+
+// Run the server!
+const start = async () => {
+    try {
+        await fastify.listen(port)
+    } catch (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+}
+start()
