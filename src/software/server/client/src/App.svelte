17f617884455ed4738618df616bdf42b3fe1a75e
fvilante <fvilante1@gmail.com>
2021-12-22 17:03:50 -0300

    some experiments with svelte and tailwind

--- src/software/server/client/src/App.svelte
+++ src/software/server/client/src/App.svelte
@@ -1,7 +1,10 @@
 <script lang='ts'>
-    
+
     import { runServerCommunication } from './server-communication';
+    import Button from './components/Button.svelte'
     import {onMount } from 'svelte'
+    import Array from './components/Array.svelte';
+    import Hero from './components/Hero.svelte';
     
     
     let count: number = 0
@@ -15,4 +18,18 @@
     })
 
 </script>
+
+<div>
+    <Hero> </Hero>
+</div>
+
+<div class='flex'> 
+    <Button caption='Start'> 
+        <h1> olha vamos colocar algo aqui? </h1>
+    </Button>
+    <Button caption='Stop'> </Button>
+    <Button caption='Forcar Referencia'> </Button>
+    <Array> </Array>
+</div>
+
 
---
+++ src/software/server/client/src/components/Button.svelte
@@ -0,0 +1,9 @@
+<script> 
+
+    export let caption;
+
+</script>
+
+<div class="bg-slate-900 border-1 border-red-700 "> 
+    <p class="m-2 border-4 border-red-700 bg-slate-100"> {caption} </p>
+</div>
