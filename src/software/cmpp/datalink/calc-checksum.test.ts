4680ab77b66281469f924e83d98210374743abe4
fvilante <fvilante1@gmail.com>
2021-12-27 11:27:34 -0300

    added unit test for cmpp checksum match

---
+++ src/software/cmpp/datalink/calc-checksum.test.ts
@@ -0,0 +1,49 @@
+import { calcChecksum_ } from "./calc-checksum"
+import { StartByteNum } from "./core-types"
+
+describe('basic tests', () => {
+
+    it('Can calc checksum of simple data', async () => {
+        //prepare
+        const obj = [1,2,3,4]
+        const startByte: StartByteNum = 2
+        const expected = 241
+        //act
+        const actual = calcChecksum_(obj, startByte)
+        //check
+        expect(actual).toEqual(expected)
+    })
+
+    it('Can calc checksum which is equal to ESC', async () => {
+        //prepare
+        const obj = [1,2,3,4,(241-27)]
+        const startByte: StartByteNum = 2
+        const expected = 27
+        //act
+        const actual = calcChecksum_(obj, startByte)
+        //check
+        expect(actual).toEqual(expected)
+    })
+
+    it('Can calc checksum which is equal to ESC in all startBytes', async () => {
+        //prepare
+        const obj = [1,2,3,4,(241-27)]
+        const master: StartByteNum = 2
+        const slave: StartByteNum = 6
+        const slaveError: StartByteNum = 21
+        const master_ = obj
+        const slave_ = [...obj, 256-(6-2)]
+        const slaveError_ = [...obj, 256-(21-2)]
+        const expected = 27
+        //act
+        const actual1 = calcChecksum_(master_, master)
+        const actual2 = calcChecksum_(slave_, slave)
+        const actual3 = calcChecksum_(slaveError_, slaveError)
+        //check
+        expect(actual1).toEqual(expected)
+        expect(actual2).toEqual(expected)
+        expect(actual3).toEqual(expected)
+    })
+
+
+})
