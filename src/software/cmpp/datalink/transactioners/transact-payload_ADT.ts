1647b8ff738ba4f3e2262fa88c9e831047f86869
fvilante <fvilante1@gmail.com>
2022-01-06 10:48:14 -0300

    introduced an simple ACK byte verification

--- src/software/cmpp/datalink/transactioners/transact-payload_ADT.ts
+++ src/software/cmpp/datalink/transactioners/transact-payload_ADT.ts
@@ -4,6 +4,7 @@ import { Result, Result_ } from "../../../adts/result";
 import { PortOpened } from "../../../serial";
 import { PayloadCore } from "../core/payload";
 import { payloadTransaction_CB, TimeoutErrorEvent, TransactErrorEvent } from "./payload-transact-cb";
+import { ACK } from "../core/core-types";
 
 //we are very safe because we deal with open/close of the port and not only data transfer
 
@@ -30,16 +31,21 @@ export const transactPayload_ADT = (argument: TransactPayloadArgument_ADT): Futu
                 resolve(fail(err))
             },
             onSuccess: (event, header) => {
-                //TODO: Implement a better checksum error detection
+                //TODO: Implement a better checksum error detection and NACK detection (REMOVE this error throwing!! )
                 const { frameInterpreted } = event
                 const expectedChecksum = frameInterpreted.expectedChecksum
                 const actualChecksum = frameInterpreted.checkSum[0]
                 const isValidChecksum = expectedChecksum === actualChecksum
-                if(isValidChecksum) {
+                const slaveStartByte = frameInterpreted.startByte[0]
+                const isAck = slaveStartByte === ACK
+                if(isValidChecksum && isAck) {
                     resolve(ok(frameInterpreted))
-                } else /* invalid checksum*/ {
+                } else if(!isValidChecksum) {
                     //TODO: remove this ugly throw and use 'fail' function above. 
                     throw new Error('Invalid checksum response from slave')
+                } else if(!isAck) {
+                    //TODO: remove this ugly throw and use 'fail' function above. 
+                    throw new Error('Slave responded a well-formed but NON-ACK frame')
                 }
                 
             },
