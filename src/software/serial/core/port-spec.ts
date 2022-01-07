f9e92ed8281461713dd655996aecbf354b044ca8
fvilante <fvilante1@gmail.com>
2022-01-07 10:47:29 -0300

    improved type & add comment

--- src/software/serial/core/port-spec.ts
+++ src/software/serial/core/port-spec.ts
@@ -1,10 +1,10 @@
 import { BaudRate } from './baudrate';
+import { PortInfo } from './port-info';
 
 //TODO: In future make it a Validated<string> or something similar.
 
 
-export type PortPath = string; // ie: 'com1', 'com2', etc...
-
+export type PortPath = PortInfo['path']; // ie: 'com1', 'com2', etc... in Microsoft Windows. //NOTE: In Unix-like operation systems this strings have different structure (ie: 'USBA1', 'USBA2', etc...)
 
 export type PortSpec = {
   readonly path: PortPath;
