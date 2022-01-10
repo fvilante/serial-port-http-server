a65196f6e805efc13fb730b2ea6b7344698274d5
fvilante <fvilante1@gmail.com>
2022-01-10 15:21:40 -0300

    example to test reference proccess

--- src/software/machine/examples/example-05.ts
+++ src/software/machine/examples/example-05.ts
@@ -1,40 +1,69 @@
-import { Moviment } from "../../cmpp/controlers/core";
-import { Pulses } from "../../cmpp/physical-dimensions/base";
+import { Kinematics, Moviment } from "../../cmpp/controlers/core";
+import { Position, Pulses } from "../../cmpp/physical-dimensions/base";
 import { PulsesPerTick, PulsesPerTickSquared } from "../../cmpp/physical-dimensions/physical-dimensions";
+import { CMPP00LG } from "../../cmpp/transport/memmap-CMPP00LG";
 import { makeTunnel } from "../../cmpp/transport/tunnel";
 import { random } from "../../core/utils";
 import { SingleAxis } from "../single-axis";
-import { randomMoviment as getRandomMoviment } from "./example-04";
 
 let axisCounter = 0
 let allCatchedErrors: readonly any[] = []
 let iterationConunter = 0
 
+//const itor = getRandomMoviment()
+const port = 'com50'
+const tunnel = makeTunnel(port, 9600, 0)
+
+const initialize_Axis_At_Random_Parameters = async (axis: SingleAxis) => {
+    //configure
+    const p1: Position = Pulses(500)
+    const k1: Kinematics = {
+        speed: PulsesPerTick(500),
+        acceleration: PulsesPerTickSquared(5000)
+    }
+    //do
+    try {
+        await axis.powerOff()
+        await axis.initialize({endPosition: p1, reference: k1})
+    } catch (err) {
+        console.log('**** Error Detected *************')
+        console.log(err)
+        allCatchedErrors = [...allCatchedErrors, err]
+    }
+    
+}
+
+const report = () => {
+    console.log(`******************** ERRORS REPORT ************************************`)
+    console.log(`totalErrors=${allCatchedErrors.length}`)
+    console.log(`errorsList=`, allCatchedErrors)
+    console.log()
+}
+
+const goAnyWhere = async (axis: SingleAxis):Promise<void> => {
+    const transportLayer = CMPP00LG(tunnel)
+    const { set } = transportLayer
+    const p2: Position = Pulses(random(500, 2300))
+    await set('Posicao final', p2)
+    await axis.startSerial()
+    await axis.waitToStop()
+}
 
 const main = async () => {
-    const port = 'com50'
-    const axis = new SingleAxis(makeTunnel(port, 9600, 0),`Eixo_Teste_#${axisCounter++}`)
-    const tolerance = [Pulses(3), Pulses(3)] as const// lower and upper bound respectively
 
-    const itor = getRandomMoviment()
+    setInterval( () => report(), 25000)
 
-    let currentPosition_: number | undefined = undefined
+    const axis1 = new SingleAxis(tunnel,`Eixo_Teste_#${axisCounter++}`)
+
+    let c = 0
     while(true) {
-        const x = itor.next()
-        if (x.done) break;
-        const nextMoviment = x.value
-        iterationConunter++
-        // body
-        try {
-            currentPosition_ = (await axis.goto(nextMoviment, tolerance)).value
-        } catch (err) {
-            allCatchedErrors = [...allCatchedErrors, [port, err]]
-            console.log({port, err})
-        }
-
+        console.log(`interação=${c++}`)
+        await initialize_Axis_At_Random_Parameters(axis1)
+        await goAnyWhere(axis1)
     }
-     
-    await axis.powerOff()
+
+
+    
 }
 
 
