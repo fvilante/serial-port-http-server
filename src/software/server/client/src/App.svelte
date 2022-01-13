556e290b4c42ada85773d149d3c093f6e8caf053
fvilante <fvilante1@gmail.com>
2022-01-13 15:30:05 -0300

    added button controls

--- src/software/server/client/src/App.svelte
+++ src/software/server/client/src/App.svelte
@@ -23,8 +23,41 @@
 
 </script>
 
+<style>
+
+    button {
+        display: inline-block;
+        padding: 1em;
+        margin: 1em;
+        border: 1px solid black;
+        transition: all 0.2s
+    }
+
+    button:hover {
+        color: azure;
+        font-style: oblique;
+    }
+
+    button.start:hover {
+        background-color: darkgreen;
+    }
+
+    button.stop:hover {
+        background-color: darkred;
+    }
+
+    button.reference:hover {
+        color: black;
+        background-color: yellow;
+    }
+
+</style>
+
 
 <div class='flex'> 
+    <button class='start'>start</button>
+    <button class='stop'>stop</button>
+    <button class='reference'>reference</button>
     
 </div>
 
