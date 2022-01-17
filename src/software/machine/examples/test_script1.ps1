2d3c9258ac89860261ce6b4711f6ea8854c8668f
fvilante <fvilante1@gmail.com>
2022-01-17 13:30:53 -0300

    introduced powershell script for test

--- src/software/machine/examples/test_script1.ps1
+++ src/software/machine/examples/test_script1.ps1
@@ -1,21 +1,26 @@
 $current_dir = Get-Location
 $working_dir = "C:\Repositorios\StageArea\NodeSerialServer\src\software\machine\examples"
 
-Write-Host 'Programando Job'
+Write-Host Programando Job
 $job = Start-job { 
     #Set-Location $working_dir
     Write-Host "Iniciando job..."
     npx ts-node .\mexample-03.ts
     Write-Host "Job finalizado"
 }
-Write-Host 'Ok'
-Write-Host 'Aguardando a finalizacao do job...'
-Wait-Job -Job $job
-$r = Receive-Job -Job $job -Keep | Select-Object -Last 30
-Write-Host $r
-Write-Host '-------------'
-Write-Host 'Para o resultado das ultimas 30 linhas, digite: $r'
+Write-Host Ok
+Write-Host Aguardando a finalizacao do job...
+try {
+    Wait-Job -Job $job
+}
+finally {
+    $r = Receive-Job -Job $job -Keep | Select-Object -Last 30    
+} 
 
+
+Write-Host $r
+Write-Host -------------
+Write-Host Para o resultado das ultimas 30 linhas, digite: $r e/ou Get-Error
 
 
 
