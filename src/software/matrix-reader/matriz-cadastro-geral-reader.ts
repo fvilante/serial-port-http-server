cc925e6b47642f7767a1499845cf86c688897a49
fvilante <fvilante1@gmail.com>
2022-01-18 15:57:56 -0300

    folder renaming

--- src/software/matrix-reader/matriz-cadastro-geral-reader.ts
+++ src/software/matrix-reader/matriz-cadastro-geral-reader.ts
@@ -1,7 +1,7 @@
 import { readFile,  } from 'fs'
 import { Matriz } from './matrizes-conhecidas'
 import { Matriz3, matrizConverter_3_1 } from './matrizes-conhecidas-converter'
-import { BarCode } from '../barcode-reader/barcode-stream'
+import { BarCode } from '../barcode/barcode-stream'
 
 const CurrentDirectory = process.cwd()
 const FILE = 'cadastro_geral.json'
