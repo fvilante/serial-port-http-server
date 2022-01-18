c25ef34c42792cef0dbb1c0c9cbe0a33d9e757f2
fvilante <fvilante1@gmail.com>
2022-01-18 15:42:07 -0300

    comment

--- src/software/keyboard/read-keyboard-async.ts
+++ src/software/keyboard/read-keyboard-async.ts
@@ -13,10 +13,12 @@ export type KeyboardEvent = {
 export type KeyboardEventEmitter = (consumer: (f: KeyboardEvent) => void) => void
 // Represents a stream of all keystrokes pressed (ctrl+c stops the stream and finish the proccess)
 export const keyboardEventEmiter:KeyboardEventEmitter = (consumer: (_:KeyboardEvent) => void):void => {
+    //TODO: Decide if below instantiation shoud be inside or outside this
     readline.emitKeypressEvents(process.stdin);
     process.stdin.setRawMode(true);
 
     process.stdin.on('keypress', (str , key) => {
+        //TODO: Probably I should remove/extract this if statement. This are compling keystroke aquisition with keystroke proccessing
         //ctrl-c ( end of text )
         if ( key.sequence === '\u0003' ) {
             console.log('Finalizando programa... ok!')
